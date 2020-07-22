b4c931d920e9a66a52d747e5ae48e664fb9c961d
franckyp1 <franckyp1+rapports_git>
2020-07-22 19:00:27 +0200

    Ajoutez des fichiers projet.

---
+++ TPJeu/Joueur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPJeu
+{
+    class Joueur
+    {
+        int PointsVie { get; }
+        bool EstVivant { get; }
+        public Joueur(int pointsvie)
+        {
+            PointsVie = pointsvie;
+        }
+
+        public void Attaque(Monstre monstre)
+        {
+
+        }
+
+        public void SubitDegats(int degatsSubits)
+        {
+
+        }
+    }
+}
